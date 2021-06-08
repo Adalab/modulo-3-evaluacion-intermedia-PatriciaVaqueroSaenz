a10fe3433bcb6b8921f3ee228b37085f54cb1e08
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-08 09:33:55 +0200

    add pokemons types to list

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,10 +4,18 @@ import '../stylesheet/App.scss';
 
 const Pokemon = (props) =>{
 
+    const renderTypes = () =>{
+        return props.pokemon.types.map(
+          (type) =>{
+            return <li>{type}</li>
+          }
+        );
+      };
     return (
         <article>
             <img src={props.pokemon.url} alt={props.pokemon.name} />
             <h3>{props.pokemon.name}</h3>
+            <ul>{renderTypes()}</ul>
         </article>
     );
     
