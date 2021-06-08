fe4a06422c72e806706179a8165192d7841e88d0
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-08 10:19:27 +0200

    add scss

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PokeList from "./PokeList";
 import data from "../data/data.json";
 import "../stylesheet/App.scss";
+import image from "../images/pokeball.png"
 
 const App = () => {
   const [pokemonInfo] = useState(data);
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <header className="header__section">
         <h1 className="title__header">Mi lista de pokemon</h1>
+        <img className='image__header' src={image} alt="pokeball" />
       </header>
       <main className="main__section">
         <section className="pokelist__section">
