79ba26e58c5a6beb72e85b836266345de057ab06
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-08 09:29:25 +0200

    add pokemons name and photo to list

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,7 +6,8 @@ const Pokemon = (props) =>{
 
     return (
         <article>
-            
+            <img src={props.pokemon.url} alt={props.pokemon.name} />
+            <h3>{props.pokemon.name}</h3>
         </article>
     );
     
